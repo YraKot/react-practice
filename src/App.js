8ffdef1602a5e7bc10912f1e99c947dacb4eab09
Yra Kot <y.kitsera@gmail.com>
2020-04-25 23:40:00 +0300

    add localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './TodoList'
 
 export const App = () => {
-	const [todos, setTodo] = useState([
-		{ id: 1, title: 'First todo', completed: false },
-		{ id: 2, title: 'Second todo', completed: true },
-	]);
-
+	const [todos, setTodos] = useState([]);
 	const [todoTitle, setTodoTitle] = useState('');
+
+	useEffect(() => {
+		const raw = JSON.parse(localStorage.getItem('todos')) || [];
+		setTodos(raw);
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
 
 	const handleChange = (e) => {
 		setTodoTitle(e.target.value);
@@ -15,7 +20,7 @@ export const App = () => {
 
 	const addTodo = (e) => {
 		if (e.key === 'Enter'){
-			setTodo([
+			setTodos([
 				...todos,
 				{
 					id: Date.now(),
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,10 +2,13 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-export const TodoList = ({todos}) => (
+export const TodoList = ({todos}) => {
+  console.log('todos', todos);
+  
+  return (
     <ul>
       {todos.map(item => <TodoItem key={item.id} {...item} />)}
     </ul>
-);
+)};
 
 export default TodoList;
